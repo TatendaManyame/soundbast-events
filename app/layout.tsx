aab1b191b03ebdbc891ae4959d969ffb1cbd2b88
TatendaManyame <bazeltatendamanyame@gmail.com>
2026-05-15 04:08:33 +0400

    Added Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { ReactNode } from 'react';
 
@@ -11,6 +12,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Navbar />
         {children}
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   )
